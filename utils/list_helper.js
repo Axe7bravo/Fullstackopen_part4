59ab79c0a0dff1fd6aa256b4434fba048ee697eb
Thato Nkoko <thato.dice@gmail.com>
2025-03-27 00:15:28 +0200

    Add Exercise 4.3 and 4.4
    Helper Functions and Unit Tests, step 1.[4.3].
    Helper Functions and Unit Tests, step 2 [4.4]

---
+++ utils/list_helper.js
@@ -0,0 +1,13 @@
+// utils/list_helper.js
+const dummy = (blogs) => {
+    return 1;
+};
+
+const totalLikes = (blogs) => {
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+};
+
+module.exports = {
+    dummy,
+    totalLikes
+};
