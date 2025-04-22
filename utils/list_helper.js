ffc3a00d66de4b39bde0bf1d0600dc868a7210e9
Thato Nkoko <thato.dice@gmail.com>
2025-04-22 19:28:43 +0200

    Fix linting errors

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,55 +1,56 @@
 // utils/list_helper.js
 
 const _ = require('lodash')
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
-    return 1
+  return 1
 }
 
 const totalLikes = (blogs) => {
-    return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
 const favoriteBlog = (blogs) => {
-    if (blogs.length === 0) {
-        return null
-    }
+  if (blogs.length === 0) {
+    return null
+  }
 
-    return blogs.reduce((favorite, current) => {
-        return current.likes > favorite.likes ? current : favorite
-    })
+  return blogs.reduce((favorite, current) => {
+    return current.likes > favorite.likes ? current : favorite
+  })
 }
 
 const mostBlogs = (blogs) => {
-    if (blogs.length === 0) {
-        return null
-    }
+  if (blogs.length === 0) {
+    return null
+  }
 
-    const authors = _.countBy(blogs, 'author')
-    const topAuthor = _.maxBy(Object.keys(authors), (author) => authors[author])
+  const authors = _.countBy(blogs, 'author')
+  const topAuthor = _.maxBy(Object.keys(authors), (author) => authors[author])
 
-    return topAuthor ? { author: topAuthor, blogs: authors[topAuthor] } : null
+  return topAuthor ? { author: topAuthor, blogs: authors[topAuthor] } : null
 }
 
 const mostLikes = (blogs) => {
-    if (blogs.length === 0) {
-        return null
-    }
+  if (blogs.length === 0) {
+    return null
+  }
 
-    const authorLikes = _(blogs)
-        .groupBy('author')
-        .map((objs, key) => ({
-            author: key,
-            likes: _.sumBy(objs, 'likes'),
-        }))
-        .maxBy('likes')
+  const authorLikes = _(blogs)
+    .groupBy('author')
+    .map((objs, key) => ({
+      author: key,
+      likes: _.sumBy(objs, 'likes'),
+    }))
+    .maxBy('likes')
 
-    return authorLikes || null
+  return authorLikes || null
 }
 
 module.exports = {
-    dummy,
-    totalLikes,
-    favoriteBlog,
-    mostBlogs,
-    mostLikes
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
